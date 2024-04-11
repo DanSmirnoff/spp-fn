ebdeb1993cbd49e37a17bd7eae23ff4e91db8c80
DimaTivator <andriianov164@gmail.com>
2024-04-11 23:03:28 +0300

    ADD: model_infer function (untested)

--- backend_fastapi/fastapi_service/services/model_infer.py
+++ backend_fastapi/fastapi_service/services/model_infer.py
@@ -1,9 +1,33 @@
 import logging
+
+import pandas as pd
+from catboost import CatBoostRegressor
 from backend_fastapi.fastapi_service.services import DataLoader
 
 logger = logging.getLogger(__name__)
 
 
-def infer_model() -> float:
-    pass
+def infer_model(df: pd.DataFrame) -> bool:
+    """
+    receives dataframe with dates
+    makes predictions and updates model_pred.csv
+    :param df:
+    :return:
+    """
+    model = CatBoostRegressor()
+    model.load_model('backend_fastapi/data/catboost_model')
+
+    dates = df['date']
+    df = df.drop(columns=['date'])
+    y_pred = model.predict(df)
+
+    df_pred = pd.DataFrame({'date': dates, 'y_pred': y_pred})
+
+    df_old = pd.read_csv('backend_fastapi/data/model_pred.csv')
+    df = pd.concat([df_old, df_pred])
+    df = df.drop_duplicates(subset=['date'])
+
+    df.to_csv('backend_fastapi/data/model_pred.csv', index=False)
+
+    return True
 
