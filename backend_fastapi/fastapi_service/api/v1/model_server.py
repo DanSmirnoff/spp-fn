e71a5b34a4a85dc3f63d5243aa16118f22e717c9
DimaTivator <andriianov164@gmail.com>
2024-04-11 23:15:08 +0300

    ADD: fastapi methods (untested)

--- backend_fastapi/fastapi_service/api/v1/model_server.py
+++ backend_fastapi/fastapi_service/api/v1/model_server.py
@@ -1,5 +1,7 @@
 import logging
 
+import pandas as pd
+from datetime import datetime
 from fastapi import APIRouter
 
 logger = logging.getLogger()
@@ -7,10 +9,16 @@ router = APIRouter()
 
 
 @router.post("/predict_item")
-async def predict_item():
-    return {"prediction"}
+async def get_predictions(dateFrom: str, dateTo: str):
+    df = pd.read_csv('backend_fastapi/data/model_pred.csv')
+    dateFrom = datetime.strptime(dateFrom, '%Y-%m-%d')
+    dateTo = datetime.strptime(dateTo, '%Y-%m-%d')
+    df = df.loc[(df['date'].dt.date >= dateFrom) & (df['date'].dt.date <= dateTo)]
+    return {'date': df['date'], 'prediction': df['pred']}
 
 
-@router.post("/predict_items")
-async def predict_items():
-    return {"prediction"}
+@router.post("/news")
+async def predict_items(num: int):
+    df = pd.read_csv('backend_fastapi/data/lenta_news.csv')
+    df = df.loc[len(df) - num:]
+    return {'title': df['title'], 'date': df['date'], 'url': df['url']}
